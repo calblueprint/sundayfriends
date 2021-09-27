6dd16d3baaa3eb9a3dc8481d3af3a87887620bce
Jacob Kim <jacobjk01@berkeley.edu>
2021-09-26 18:46:29 -0700

    jacob is cool

--- pages/Transactions/index.tsx
+++ pages/Transactions/index.tsx
@@ -2,7 +2,9 @@ import Layout from "../../components/Layout/Layout";
 
 const TransactionsPage: React.FunctionComponent = () => {
     return(
-        <Layout title='Transactions'/>
+        <Layout title='Transactions'>
+            <h1>i literally don't understand why jacob is so cool</h1>
+        </Layout>
     );
 }
 
