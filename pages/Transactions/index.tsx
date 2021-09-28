6019218e2f58defa02e07e6844d5bf51476193e1
Jacob Kim <jacobjk01@berkeley.edu>
2021-09-28 13:52:42 -0700

    Tabpanel component and basic tab styles

--- pages/Transactions/index.tsx
+++ pages/Transactions/index.tsx
@@ -3,44 +3,11 @@ import Layout from "../../components/Layout/Layout";
 import PropTypes from 'prop-types';
 import { Grid, Button, Tabs, Tab, Box } from "@mui/material";
 import styles from './Transactions.module.css';
+import { TabPanel } from '../../components/TabPanel';
 
 const TransactionsPage: React.FunctionComponent = () => {
 
-    function TabPanel(props) {
-        const { children, value, index, ...other } = props;
-      
-        return (
-          <div
-            role="tabpanel"
-            hidden={value !== index}
-            className={styles.box}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-          >
-            {value === index && (
-              <Box sx={{ p: 3 }} className={styles.tabbox}>
-                <h3>{children}</h3>
-              </Box>
-            )}
-          </div>
-        );
-      }
-      
-      TabPanel.propTypes = {
-        children: PropTypes.node,
-        index: PropTypes.number.isRequired,
-        value: PropTypes.number.isRequired,
-      };
-      
-      function a11yProps(index) {
-        return {
-          id: `simple-tab-${index}`,
-          'aria-controls': `simple-tabpanel-${index}`,
-        };
-      }
-
-    function BasicTabs() {
+    const BasicTabs = () => {
         const [value, setValue] = React.useState(0);
         
         const handleChange = (event, newValue) => {
@@ -48,12 +15,12 @@ const TransactionsPage: React.FunctionComponent = () => {
         };
         
         return (
-            <Box sx={{ width: '100%', height: '60%' }}>
+            <Box sx={{ width: '100%', maxHeight: '62vh' }}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="History" {...a11yProps(0)} />
-                <Tab label="Redemptions" {...a11yProps(1)} />
-                <Tab label="Earnings" {...a11yProps(2)} />
+                <Tab label="History" className={value==0 ? styles['seltab'] : styles['tab']}/>
+                <Tab label="Redemptions" className={value==1 ? styles['seltab'] : styles['tab']}/>
+                <Tab label="Earnings" className={value==2 ? styles['seltab'] : styles['tab']}/>
                 </Tabs>
             </Box>
                 <TabPanel value={value} index={0}>
@@ -71,15 +38,15 @@ const TransactionsPage: React.FunctionComponent = () => {
       
     return(
         <Layout title='Transactions'>
-            <Grid container className={styles.container}>
+            <Grid container rowSpacing={1} className={styles.container}>
                 <Grid item xs={8}>
-                    <h2 className={styles.h2}>TRANSACTIONS</h2>
+                    <h2 className={styles['h2']}>TRANSACTIONS</h2>
                 </Grid>
                 <Grid item xs={2}>
-                    <Button className={styles.button}>Add Single Transaction</Button>
+                    <Button className={styles['button']}>Add Single Transaction</Button>
                 </Grid>
                 <Grid item xs={2}>
-                    <Button className={styles.button}>Upload Bulk Transactions</Button>
+                    <Button className={styles['button']}>Upload Bulk Transactions</Button>
                 </Grid>
                 {BasicTabs()}
             </Grid>
