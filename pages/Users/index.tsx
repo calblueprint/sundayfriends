bf61a13495ea85271e2ec97b765fd46c918dc149
sydbui <sydbui@berkeley.edu>
2021-09-28 00:52:28 -0700

    add basic css colors and userAccounts

--- pages/Users/index.tsx
+++ pages/Users/index.tsx
@@ -1,8 +1,13 @@
 import Layout from "../../components/Layout/Layout";
+import styles from "./UserAccounts.module.css"
 
 const UsersPage: React.FunctionComponent = () => {
     return(
-        <Layout title='Users'/>
+        <Layout title='Users'>
+            <div className={styles['container']}>
+            <h1>User Accounts</h1>
+            </div>
+        </Layout>
     );
 }
 
