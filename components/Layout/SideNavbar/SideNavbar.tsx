4b0a2c2b7f0fc94dea079340a0fc6c27f7272e82
Cindy <cindyzhang7@berkeley.edu>
2021-09-28 23:07:27 -0700

    changed sidebar names and routes

--- components/Layout/SideNavbar/SideNavbar.tsx
+++ components/Layout/SideNavbar/SideNavbar.tsx
@@ -1,52 +1,69 @@
-import React from 'react';
-import {
-    Drawer,
-    List,
-    ListItem,
-    ListItemText,
-} from '@mui/material';
-import { useRouter } from 'next/router';
-import styles from './SideNavbar.module.css';
+import React from "react";
+import { Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { useRouter } from "next/router";
+import styles from "./SideNavbar.module.css";
 
 const SideNavbar: React.FunctionComponent = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <Drawer
-            anchor='left'
-            variant='permanent'
-            className={styles['sidenav']}
-            classes={{
-                paper: styles['sidenav-paper']
-            }}
+  return (
+    <Drawer
+      anchor="left"
+      variant="permanent"
+      className={styles["sidenav"]}
+      classes={{
+        paper: styles["sidenav-paper"],
+      }}
+    >
+      <List className={styles.list}>
+        <ListItem
+          button
+          key="logo"
+          onClick={() => {
+            router.push("/");
+          }}
         >
-            <List className={styles.list}>
-                <ListItem 
-                    button
-                    key='logo'
-                    onClick={() => {
-                        router.push('/')
-                    }}
-                    disableRipple={true}
-                >
-                    <ListItemText className={styles['item']} primary={'Sunday Friends'} />
-                </ListItem>
-            {['Users', 'Admins', 'Transactions', 'Inventory'].map((text) => (
-                // iterate through items in the list to generate tabs for the navbar/drawer
-                <ListItem 
-                    button
-                    key={text} 
-                    onClick={() => {
-                        router.push(`/${text}`)
-                    }}
-                    disableRipple={true}
-                >
-                <ListItemText className={styles['item']} primary={text} />
-                </ListItem>
-            ))}
-            </List>
-        </Drawer>
-    );
+          <ListItemText className={styles["item"]} primary={"Sunday Friends"} />
+        </ListItem>
+        <ListItem
+          button
+          key={"users"}
+          onClick={() => {
+            router.push(`users`);
+          }}
+        >
+          <ListItemText className={styles["item"]} primary={"Users"} />
+        </ListItem>
+        <ListItem
+          button
+          key={"admins"}
+          onClick={() => {
+            router.push(`/admins`);
+          }}
+        >
+          <ListItemText className={styles["item"]} primary={"Admins"} />
+        </ListItem>
+        <ListItem
+          button
+          key={"transactions"}
+          onClick={() => {
+            router.push(`/transactions`);
+          }}
+        >
+          <ListItemText className={styles["item"]} primary={"Transactions"} />
+        </ListItem>
+        <ListItem
+          button
+          key={"inventory"}
+          onClick={() => {
+            router.push(`/inventory`);
+          }}
+        >
+          <ListItemText className={styles["item"]} primary={"Inventory"} />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 };
 
 export default SideNavbar;
--- components/TabPanel/TabPanel.tsx
+++ components/TabPanel/TabPanel.tsx
@@ -1,30 +1,29 @@
-import styles from '../../pages/Transactions/Transactions.module.css';
+import styles from "../../pages/Transactions/Transactions.module.css";
 import { Box } from "@mui/material";
 
 type TabPanelProps = {
-    index: number,
-    value: number,
+  index: number;
+  value: number;
 };
 
 export const TabPanel: React.FunctionComponent<TabPanelProps> = ({
-    children, 
-    index, 
-    value
+  children,
+  index,
+  value,
 }) => {
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        className={styles['box']}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }} className={styles['tabbox']}>
-            <h3>{children}</h3>
-          </Box>
-        )}
-      </div>
-    );
-}
-  
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      className={styles["box"]}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }} className={styles["tabbox"]}>
+          <h3>{children}</h3>
+        </Box>
+      )}
+    </div>
+  );
+};
