0e6967820f9d87dfebdf2b7367aaa72a17364cee
Cindy <cindyzhang7@berkeley.edu>
2021-09-29 14:26:02 -0700

    changed profile route case

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 import Layout from "../../components/Layout/Layout";
 import styles from "./Profile.module.css";
 
 const ProfileSettingsPage: React.FunctionComponent = () => {
-    
-    return (
-        <Layout title = 'Profile'>
-            <main className = {styles.main}>
-                Profile Settings
-            </main>
-        </Layout>
-    );
-}
+  return (
+    <Layout title="Profile">
+      <main className={styles.main}>Profile Settings</main>
+    </Layout>
+  );
+};
 
 export default ProfileSettingsPage;
