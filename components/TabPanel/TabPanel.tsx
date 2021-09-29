0898c8defd7c76c41ff0e873c2f679731b988bec
Jacob Kim <jacobjk01@berkeley.edu>
2021-09-29 01:32:50 -0700

    fixed spacing and styling, and added basic list ui

--- components/TabPanel/TabPanel.tsx
+++ components/TabPanel/TabPanel.tsx
@@ -20,8 +20,8 @@ export const TabPanel: React.FunctionComponent<TabPanelProps> = ({
         aria-labelledby={`simple-tab-${index}`}
       >
         {value === index && (
-          <Box sx={{ p: 3 }} className={styles['tabbox']}>
-            <h3>{children}</h3>
+          <Box sx={{ p: 3 }} className={styles['tab-box']}>
+            {children}
           </Box>
         )}
       </div>
--- pages/Transactions/index.tsx
+++ pages/Transactions/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Layout from "../../components/Layout/Layout";
-import { Grid, Button, Tabs, Tab, Box } from "@mui/material";
+import { Grid, Button, Tabs, Tab, Box, SvgIcon, List, ListItem} from "@mui/material";
+import Icon from '@mui/material/Icon';
 import styles from './Transactions.module.css';
 import { TabPanel } from '../../components/TabPanel/TabPanel';
-
+import addIcon from '../../public/plus_circle_outline.png';
 
 const TransactionsPage: React.FunctionComponent = () => {
 
@@ -14,27 +15,92 @@ const TransactionsPage: React.FunctionComponent = () => {
             setValue(newValue);
         };
 
-        const renderHistory: React.ReactNode = () => {
+        const renderFilterHeader = () => {
+            return (
+                <div className={styles['filter-row']}>
+                    <div>
+                        Sep 5 - Sep 12
+                    </div>
+                    <div>
+                        Filters
+                    </div>
+                    <div>
+                        Filter Name
+                    </div>
+                    <div>
+                        Search
+                    </div>
+                </div>
+            )
+        }
 
+        const renderCategoryHeader = () => {
+            return (
+                <div className={styles['section-header']}>
+                    <div>Date</div>
+                    <div>User</div>
+                    <div>FID</div>
+                    <div>Admin</div>
+                    <div>Action</div>
+                    <div>Message</div>
+                    <div>Change</div>
+                </div>
+            )
+        }
+
+        const renderHistory = () => {
+            return(
+                <List className={styles['list']}>
+                    <ListItem className={styles['list-item']}>
+                        <div>09/01/21</div>
+                        <div>Firstname Lastname</div>
+                        <div>H1234</div>
+                        <div>Firstname Lastname</div>
+                        <div>Redeem</div>
+                        <div>Short messaging</div>
+                        <div>-10.00</div>
+                    </ListItem>
+                    <ListItem className={styles['list-item']}>
+                        Hi Hello
+                    </ListItem>
+                    <ListItem className={styles['list-item']}>
+                        Hi Hello
+                    </ListItem>
+                    <ListItem className={styles['list-item']}>
+                        Hi Hello
+                    </ListItem>
+                </List>
+            )
         }
         
         return (
-            <Box sx={{ width: '100%', maxHeight: '62vh' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} indicatorColor={'white'} textColor={"inherit"} aria-label="basic tabs example">
-                <Tab label="History" className={value==0 ? styles['seltab'] : styles['tab']}/>
-                <Tab label="Redemptions" className={value==1 ? styles['seltab'] : styles['tab']}/>
-                <Tab label="Earnings" className={value==2 ? styles['seltab'] : styles['tab']}/>
+            <Box sx={{ width: '100%', maxHeight: '62%'}}>
+            <Box>
+                <Tabs value={value} onChange={handleChange} TabIndicatorProps={{style: {background: "white"}}} 
+                textColor={"inherit"} aria-label="basic tabs example">
+                <Tab label="History" className={value==0 ? styles['sel-tab'] : styles['tab']}/>
+                <Tab label="Redemptions" className={value==1 ? styles['sel-tab'] : styles['tab']}/>
+                <Tab label="Earnings" className={value==2 ? styles['sel-tab'] : styles['tab']}/>
                 </Tabs>
             </Box>
                 <TabPanel value={value} index={0}>
-                    Item One
+                    <div>
+                        {renderFilterHeader()}
+                        {renderCategoryHeader()}
+                        {renderHistory()}
+                    </div>
                 </TabPanel>
                 <TabPanel value={value} index={1}>
-                    Item Two
+                    <div>
+                        {renderFilterHeader()}
+                        {renderCategoryHeader()}
+                    </div>
                 </TabPanel>
                 <TabPanel value={value} index={2}>
-                    Item Three
+                    <div>
+                        {renderFilterHeader()}
+                        {renderCategoryHeader()}
+                    </div>
                 </TabPanel>
             </Box>
         );
@@ -44,10 +110,14 @@ const TransactionsPage: React.FunctionComponent = () => {
         <Layout title='Transactions'>
             <div className={styles['screen']}>
                 <div className={styles['container']}>
-                    <h2 className={'h2'}>TRANSACTIONS</h2>
-                    <div className={styles['buttonContainer']}>
-                        <Button className={styles['button']}>Add</Button>
-                        <Button className={styles['button']}>Upload</Button>
+                    <h2 className={styles['h2']}>TRANSACTIONS</h2>
+                    <div className={styles['button-container']}>
+                        <Button className={styles['button']}>
+                            Add
+                        </Button>
+                        <Button className={styles['button']}>
+                            Upload
+                        </Button>
                     </div>
                 </div>
                 {BasicTabs()}
