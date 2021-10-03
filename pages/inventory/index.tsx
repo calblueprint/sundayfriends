e0b33c47f9f0b2f3f7ad57d24ace2007642d9fda
Cindy <cindyzhang7@berkeley.edu>
2021-10-03 15:15:53 -0700

    dont ignore ase

---
+++ pages/inventory/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "../../components/Layout/Layout";
+
+const InventoryPage: React.FunctionComponent = () => {
+  return <Layout title="Inventory" />;
+};
+
+export default InventoryPage;
