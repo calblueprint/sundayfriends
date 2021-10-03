23d797cc3d33363805658e2179f998855bb7f2ce
danashimessele <danashimessele@berkeley.edu>
2021-10-03 14:24:54 -0700

    pulling origin

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -1,3 +1,4 @@
+import { style } from '@mui/system';
 import React from 'react';
 import Layout from "../../components/Layout/Layout";
 import styles from "./Profile.module.css";
@@ -6,9 +7,25 @@ const ProfileSettingsPage: React.FunctionComponent = () => {
     
     return (
         <Layout title = 'Profile'>
-            <main className = {styles.main}>
+            <div className = {styles.title}>
                 Profile Settings
-            </main>
+            <div className = {styles.name}>
+                Cindy Zhang
+            <hr></hr>
+            <div className = {styles.boxes}>
+                <div className = {styles.box}>
+                    <div className = {styles.subtitle}>About Info</div>
+                    <div className = {styles.boldtitle}>Role</div>  
+                    <div className = {styles.info}>Executive Director</div>
+                </div>
+                <div className = {styles.box}>
+                    <div className = {styles.subtitle}>
+                        Contact Info
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
         </Layout>
     );
 }
