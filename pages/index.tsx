8332d8510938a8d30c8524d4263f8b9097e6e3e5
Cindy Zhang <61183011+cindyxzhang@users.noreply.github.com>
2021-10-03 15:46:22 -0700

    dont ignore ase (#13)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import Layout from "../components/Layout/Layout";
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Sunday Friends Admin
-        </h1>
+        <h1 className={styles.title}>Sunday Friends Admin</h1>
       </main>
     </Layout>
-  )
+  );
 }
