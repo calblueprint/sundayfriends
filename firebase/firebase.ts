fd91104dbf22e39a7466050ae99385bda7950fab
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-10-03 22:29:15 -0700

    added firebase functions

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,6 +1,8 @@
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import "firebase/auth";
-import "firebase/firestore";
+import { getFirestore, collection, query, where, doc, getDoc, getDocs, DocumentData } from 'firebase/firestore';
+import { Transaction } from '../types/types';
+
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,6 +13,48 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-export default firebase;
+const db = getFirestore(firebaseApp);
+const transactionsCollection = collection(db, "transactions");
+
+const loadTransaction = async (doc: DocumentData): Promise<Transaction> => {
+  const adminDoc = await getDoc(doc.admin_id);
+  const familyDoc = await getDoc(doc.family_id);
+  const userDoc = await getDoc(doc.user_id);
+
+  const transaction = {
+    admin_id: adminDoc.id,
+    date: doc.date,
+    description: doc.description,
+    family_id: familyDoc.id,
+    point_gain: doc.point_gain,
+    user_id: userDoc.id
+  }
+  return transaction;
+}
+
+export const getTransaction = async (id: string): Promise<Transaction> => {
+  try {
+    const docRef = doc(db, "transactions", id);
+    const docSnap = await getDoc(docRef);
+    const loadedTransaction = await loadTransaction(docSnap.data());
+    return loadedTransaction;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+export const getAllTransactions = async (): Promise<Transaction[]> => {
+  try {
+    const dbQuery = query(transactionsCollection);
+    const querySnapshots = await getDocs(dbQuery);
+    return Promise.all(querySnapshots.docs.map(async (doc) => await loadTransaction(doc.data())));
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
+}
+
+export default firebaseApp;
---
+++ types/types.ts
@@ -0,0 +1,8 @@
+export type Transaction = {
+    admin_id: string;
+    date: Date;
+    description: string | null;
+    family_id: string;
+    point_gain: number;
+    user_id: string;
+}
