afc0641dcbd56ba11fc2475aed86c9602be739bb
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-10-07 18:06:30 -0700

    updated

--- pages/admins/index.tsx
+++ pages/admins/index.tsx
@@ -5,23 +5,22 @@ import { Grid, Button, Box } from "@mui/material";
 
 const AdminPage: React.FunctionComponent = () => {
 
-    return(
-        <Layout title='Admins'>
-          <main className={styles['main']}>
-            <Grid container className={styles['container']}>
-              <Grid item xs={8}>
-                <h2 className={styles['h2']}>ADMIN ACCOUNTS</h2>
-              </Grid>
-              <Grid item className={styles['buttons']} xs={2}>
-                  <Button className={styles['button']}>INVITE ADMIN</Button>
-              </Grid>
-              <Grid container className={styles['table']}>
-              
-              </Grid>
-            </Grid>
-          </main>
-        </Layout>
-    );
+  return (
+    <Layout title='Admins'>
+      <main className={styles['main']}>
+        <Grid container className={styles['container']}>
+          <Grid item xs={8}>
+            <h2 className={styles['h2']}>ADMIN ACCOUNTS</h2>
+          </Grid>
+          <Grid item className={styles['buttons']} xs={2}>
+            <Button className={styles['button']}>INVITE ADMIN</Button>
+          </Grid>
+          <Grid container className={styles['table']}>
+          </Grid>
+        </Grid>
+      </main>
+    </Layout>
+  );
 }
 
 export default AdminPage;
