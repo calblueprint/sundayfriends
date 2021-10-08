79d107fd623311d3b17326b26bba5db617d0903d
danashimessele <danashimessele@berkeley.edu>
2021-10-08 02:39:24 -0700

    sprint task 2 complete late my bad

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -6,28 +6,43 @@ import styles from "./Profile.module.css";
 const ProfileSettingsPage: React.FunctionComponent = () => {
     
     return (
-        <Layout title = 'Profile'>
-            <div className = {styles.header}>
-                <h2>Profile Settings</h2>
-                <h2>Cindy Zhang</h2>
-            <hr></hr>
-            <div className = {styles.boxes}>
-                <div className = {styles.box}>
-                    <div className = {styles.subtitle}>About Info</div>
-                    <br></br>
-                    <div className = {styles.boldtitle}>Role</div>
-                    <br></br>  
-                    <div className = {styles.info}>Executive Director</div>
-                </div>
-                <div className = {styles.box}>
-                    <div className = {styles.subtitle}>
-                        Contact Info
+        <body className = {styles.body}>
+            <Layout title = 'Profile'>
+                <div className = {styles.all}>
+                    <div className = {styles.header}>
+                        <h2>Profile Settings</h2>
+                    <div className = {styles.namebar}>
+                        <h2>Cindy Zhang</h2>
+                        <button className={styles.button}>Edit Profile</button>
+                    </div>
+                    <hr></hr>
+                    <div className = {styles.boxes}>
+                        <div className = {styles.box}>
+                            <div className = {styles.subheader}>About Info</div>
+                            <hr></hr>
+                            <div className = {styles.boldtitle}>Role</div>  
+                            <div className = {styles.info}>Executive Director</div>
+                            <br></br>
+                            <div className = {styles.boldtitle}>Date Last Active</div>
+                            <div className = {styles.info}> October 3, 2021 - 3:34:56 </div>
+                            <br></br>
+                            <div className = {styles.boldtitle}>Date Joined</div>
+                            <div className = {styles.info}>October 3, 2021 - 3:36:55</div>
+                        </div>
+                        <div className = {styles.box}>
+                            <div className = {styles.subheader}>Contact Info</div>
+                            <hr></hr>
+                            <div className = {styles.boldtitle}>Email Address</div>  
+                            <div className = {styles.info}>cindyzhangyayayayayay@gmail.com</div>
+                            <br></br>
+                            <div className = {styles.boldtitle}>Phone Number</div>  
+                            <div className = {styles.info}>123-456-7890</div>
+                        </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            
-            </div>
-        </Layout>
+            </Layout>
+        </body>
     );
 }
 
