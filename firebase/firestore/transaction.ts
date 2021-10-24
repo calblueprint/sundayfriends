996e2b629f885de34ffd18dee7f8c02d5e68b5be
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-10-24 15:41:50 -0700

    updated functions from modularized to namespace

--- firebase/firestore/transaction.ts
+++ firebase/firestore/transaction.ts
@@ -3,16 +3,15 @@ import 'firebase/firestore';
 import { Transaction } from '../../types/schema';
 
 const db = firebaseApp.firestore();
-const transactionsCollection = collection(db, "transactions");
+const transactionsCollection = db.collection('transactions');
 
 /**
  * Returns the transaction data from firestore with the given transactionId
  */
 export const getTransaction = async (transactionId: string): Promise<Transaction> => {
     try {
-        const docRef = doc(db, "transactions", transactionId);
-        const docSnap = await getDoc(docRef);
-        return docSnap.data() as Transaction;
+        const doc = await transactionsCollection.doc(transactionId).get();
+        return doc.data() as Transaction;
     } catch (e) {
         console.error(e);
         throw e;
@@ -25,9 +24,8 @@ export const getTransaction = async (transactionId: string): Promise<Transaction
 export const getAllTransactions = async (): Promise<Transaction[]> => {
     try {
         // query everything in the transaction collection
-        const dbQuery = query(transactionsCollection);
-        const querySnapshots = await getDocs(dbQuery);
-        return querySnapshots.docs.map((doc) => doc.data() as Transaction);
+        const allTransactions = await transactionsCollection.get();
+        return allTransactions.docs.map(doc => doc.data() as Transaction);
     } catch (e) {
         console.warn(e);
         throw e;
@@ -39,8 +37,7 @@ export const getAllTransactions = async (): Promise<Transaction[]> => {
  */
 export const addTransaction = async (transaction: Transaction) => {
     try {
-        const newTransactionRef = doc(transactionsCollection);
-        await setDoc(newTransactionRef, transaction)
+        await transactionsCollection.doc().set(transaction);
     } catch (e) {
         console.warn(e);
         throw e;
@@ -52,8 +49,7 @@ export const addTransaction = async (transaction: Transaction) => {
  */
 export const deleteTransaction = async (transactionId: string) => {
     try {
-        const transactionRef = doc(transactionsCollection, transactionId);
-        await deleteDoc(transactionRef);
+        await transactionsCollection.doc(transactionId).delete();
     } catch (e) {
         console.warn(e);
         throw e;
