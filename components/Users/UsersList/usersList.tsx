e0eb440f670b60fe483158df8eaeefa2034a1485
sydbui <57571626+sydbui@users.noreply.github.com>
2021-10-30 14:42:38 -0700

    User and Family Modal (#25)
    * Changed to Lower Case Routes and Reorganized Sidebar (#9)
    
    * changed sidebar names and routes
    
    * changed route for users
    
    * copied over
    
    * family and list view
    
    * spacing
    
    * add modal functionality
    
    * fixed spacing
    
    * spacing
    
    Co-authored-by: Cindy Zhang <61183011+cindyxzhang@users.noreply.github.com>

---
+++ components/Users/UsersList/usersList.tsx
@@ -0,0 +1,142 @@
+import {
+  Button,
+  Table,
+  TableContainer,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+} from "@mui/material";
+import styles from "./UsersList.module.css";
+import { Family, User } from "../../../types/schema";
+import UserModal from "../UserModal/userModal";
+import { useState } from "react";
+
+type UsersListProps = {
+  users: User[];
+  isFamilyPath: boolean;
+  family?: Family;
+  setIsOpenFam?: React.Dispatch<React.SetStateAction<boolean>>;
+};
+type UsersListItemProps = {
+  user: User;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+  isFamilyPath: boolean;
+};
+
+const UsersListItem: React.FC<UsersListItemProps> = ({
+  user,
+  setIsOpen,
+  setUser,
+  isFamilyPath,
+}: UsersListItemProps) => {
+  return (
+    <TableRow key={user.email} className={styles["tableRow"]}>
+      <TableCell className={`${styles["tableRow"]} ${styles["name"]}`}>
+        {user.full_name}
+      </TableCell>
+      {isFamilyPath ? null : (
+        <TableCell className={`${styles["tableRow"]} ${styles["FID"]}`}>
+          {user.family_id}
+        </TableCell>
+      )}
+      <TableCell className={`${styles["tableRow"]} ${styles["role"]}`}>
+        {user.family_head ? "Head" : "Member"}
+      </TableCell>
+      <TableCell className={`${styles["tableRow"]} ${styles["email"]}`}>
+        {user.email}
+      </TableCell>
+      <TableCell className={`${styles["tableRow"]} ${styles["balance"]}`}>
+        {user.points}
+      </TableCell>
+      <TableCell className={`${styles["tableRow"]} ${styles["lastActive"]}`}>
+        {user.last_active.toLocaleString().split(",")[0]}
+      </TableCell>
+      <TableCell className={`${styles["tableRow"]} ${styles["manage"]}`}>
+        <Button className={styles["button"]}>Delete</Button>
+        <Button className={styles["button"]}>Suspend</Button>
+        <Button
+          className={styles["viewButton"]}
+          onClick={() => {
+            setIsOpen(true);
+            setUser(user);
+          }}
+        >
+          View
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+const UsersList: React.FC<UsersListProps> = ({
+  users,
+  isFamilyPath,
+  family,
+  setIsOpenFam,
+}: UsersListProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState();
+  return (
+    <TableContainer>
+      <UserModal
+        user={user}
+        family={family}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        isFamilyPath={isFamilyPath}
+        setIsOpenFam={setIsOpenFam}
+      />
+
+      <Table stickyHeader aria-label="user table">
+        <TableHead className={styles["tableHead"]}>
+          <TableRow>
+            <TableCell className={`${styles["headingRow"]} ${styles["name"]}`}>
+              Name
+            </TableCell>
+            {isFamilyPath ? null : (
+              <TableCell className={`${styles["headingRow"]} ${styles["FID"]}`}>
+                FID
+              </TableCell>
+            )}
+            <TableCell className={`${styles["headingRow"]} ${styles["role"]}`}>
+              Role
+            </TableCell>
+            <TableCell className={`${styles["headingRow"]} ${styles["email"]}`}>
+              Email
+            </TableCell>
+            <TableCell
+              className={`${styles["headingRow"]} ${styles["balance"]}`}
+            >
+              Balance
+            </TableCell>
+            <TableCell
+              className={`${styles["headingRow"]} ${styles["lastActive"]}`}
+            >
+              Last Active
+            </TableCell>
+            <TableCell
+              className={`${styles["headingRow"]} ${styles["manage"]}`}
+            >
+              Manage
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <UsersListItem
+              key={user.email}
+              user={user}
+              setIsOpen={setIsOpen}
+              setUser={setUser}
+              isFamilyPath={isFamilyPath}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default UsersList;
--- firebase/firestore/admin.ts
+++ firebase/firestore/admin.ts
@@ -1,43 +1,43 @@
-import firebaseApp from '../firebase';
-import 'firebase/firestore';
-import { Admin } from '../../types/schema';
+import firebaseApp from "../firebase";
+import "firebase/firestore";
+import { Admin } from "../../types/schema";
 
 const db = firebaseApp.firestore();
-const adminCollection = db.collection('admins');
+const adminCollection = db.collection("admins");
 
 /**
  * Returns the admin data from firestore with the given adminId
  */
 export const getAdmin = async (adminId: string): Promise<Admin> => {
-    try {
-        const doc = await adminCollection.doc(adminId).get();
-        return doc.data() as Admin;
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
-}
+  try {
+    const doc = await adminCollection.doc(adminId).get();
+    return doc.data() as Admin;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
 
 /**
  * Adds the given admin data to firestore
  */
 export const addAdmin = async (admin: Admin) => {
-    try {
-        await adminCollection.doc().set(admin);
-    } catch (e) {
-        console.warn(e);
-        throw e;
-    }
+  try {
+    await adminCollection.doc().set(admin);
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
 };
 
 /**
  * Deletes the admin from firestore with the given adminId
  */
 export const deleteAdmin = async (adminId: string) => {
-    try {
-        await adminCollection.doc(adminId).delete();
-    } catch (e) {
-        console.warn(e);
-        throw e;
-    }
+  try {
+    await adminCollection.doc(adminId).delete();
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
 };
