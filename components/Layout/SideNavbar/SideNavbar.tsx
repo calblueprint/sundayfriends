4bcd42119344956c1de3a42b148df33bb75d4cfc
danashimessele <90217967+danashimessele@users.noreply.github.com>
2021-11-04 20:48:28 -0700

    Danashi/stylingnavbar (#27)
    * created profile page and title
    
    * created profile settings page and title
    
    * complete sprint task 1
    
    * sprint task 1
    
    * pulling origin
    
    * Changed to Lower Case Routes and Reorganized Sidebar (#9)
    
    * changed sidebar names and routes
    
    * changed route for users
    
    * pulling origin
    
    * still sprinttask2
    
    * sprint task 2 complete late my bad
    
    * Updated sprint task 2 and started 3
    
    * updates
    
    * changed components and added icons
    
    * fixed warning
    
    * navbar mostly complete
    
    * changes
    
    * changed styling and spacing a litte
    
    * fix package-lock.json
    
    * made changes from comments
    
    * fixing merge conflicts
    
    * merge conflicts
    
    * changes
    
    * changes
    
    * font and icon styling
    
    * changes
    
    Co-authored-by: Cindy Zhang <61183011+cindyxzhang@users.noreply.github.com>

--- components/Layout/SideNavbar/SideNavbar.tsx
+++ components/Layout/SideNavbar/SideNavbar.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Drawer, List, ListItem, ListItemText } from "@mui/material";
 import { useRouter } from "next/router";
 import styles from "./SideNavbar.module.css";
+import Icon from "../../../assets/Icon";
 
 const SideNavbar: React.FunctionComponent = () => {
   const router = useRouter();
@@ -17,13 +18,14 @@ const SideNavbar: React.FunctionComponent = () => {
     >
       <List className={styles.list}>
         <ListItem
-          button
           key="logo"
-          onClick={() => {
-            router.push("/");
-          }}
+          className={styles.navbar}
         >
-          <ListItemText className={styles["item"]} primary={"Sunday Friends"} />
+        <div className={styles.icon}>
+          <div className={styles.border}>
+            <Icon type="sundayfriendslogo" className={styles.SFlogo}/>
+          </div>
+        </div>        
         </ListItem>
         <ListItem
           button
@@ -31,8 +33,15 @@ const SideNavbar: React.FunctionComponent = () => {
           onClick={() => {
             router.push("/users");
           }}
+          className={styles.item}
         >
-          <ListItemText className={styles["item"]} primary={"Users"} />
+        <div className={styles.icon}>
+          <Icon type="usersnavicon" className={styles.pageicons}/>
+        </div>
+          <ListItemText 
+          disableTypography
+          className={styles["item"]} 
+          primary={"Users"} />
         </ListItem>
         <ListItem
           button
@@ -40,8 +49,15 @@ const SideNavbar: React.FunctionComponent = () => {
           onClick={() => {
             router.push("/admins");
           }}
+          className={styles.item}
         >
-          <ListItemText className={styles["item"]} primary={"Admins"} />
+        <div className={styles.icon}>
+          <Icon type="adminnavicon" className={styles.pageicons}/>
+        </div>
+          <ListItemText 
+            disableTypography
+            className={styles["item"]} 
+            primary={"Admins"} />
         </ListItem>
         <ListItem
           button
@@ -49,8 +65,15 @@ const SideNavbar: React.FunctionComponent = () => {
           onClick={() => {
             router.push("/transactions");
           }}
+          className={styles.item}
         >
-          <ListItemText className={styles["item"]} primary={"Transactions"} />
+        <div className={styles.icon}>
+          <Icon type="transactionsnavicon" className={styles.pageicons}/>
+        </div>
+          <ListItemText 
+            disableTypography
+            className={styles["item"]} 
+            primary={"Transactions"} />
         </ListItem>
         <ListItem
           button
@@ -58,8 +81,15 @@ const SideNavbar: React.FunctionComponent = () => {
           onClick={() => {
             router.push("/inventory");
           }}
+          className={styles.item}
         >
-          <ListItemText className={styles["item"]} primary={"Inventory"} />
+        <div className={styles.icon}>
+          <Icon type="inventorynavicon" className={styles.pageicons}/>
+        </div>
+          <ListItemText 
+            disableTypography
+            className={styles["item"]} 
+            primary={"Inventory"} />
         </ListItem>
       </List>
     </Drawer>
