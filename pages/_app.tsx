cad80cb89834103385ebd30832ee2e64267b9161
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-11-08 16:34:43 -0800

    remove server-side injected css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AuthProvider } from "../firebase/auth";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  useEffect(() => {
+    // Remove the server-side injected CSS
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
