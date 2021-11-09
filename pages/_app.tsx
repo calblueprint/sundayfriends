5369c69ae7966a399a097dae5f7e623a526e0b16
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-11-08 16:41:40 -0800

    run prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { AppProps } from "next/app";
 import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-
   useEffect(() => {
     // Remove the server-side injected CSS
     const jssStyles = document.querySelector("#jss-server-side");
