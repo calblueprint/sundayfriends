3a09c665ef0646a861266cb2851f692958f58d4c
Jacob Kim <jacobjk01@berkeley.edu>
2021-11-16 16:13:32 -0800

    Jacob transactions add (#23)
    * Changed to Lower Case Routes and Reorganized Sidebar (#9)
    
    * changed sidebar names and routes
    
    * changed route for users
    
    * merge conflict again oops
    
    * TransactionList component, addition of admin and user api files, populating transactions from backend
    
    * some minor changes
    
    * slight icon changes
    
    * kinda hacky fix with some hardcoded pixels to match hifigit add .
    
    * prepping firebase changes
    
    * new firebase working smoothly
    
    * family in schema and firebase calls
    
    * snake case
    
    * add modal ui
    
    * add-success
    
    * playing with textfield color
    
    * working add request
    
    * deleting extra imports
    
    * autocomplete cleaned styling
    
    * upload popover structure
    
    * family firebase update
    
    * squash cindy bug
    
    * getalltransactions now ordered and update list on add
    
    * start serversideprops
    
    * getserverprops bug
    
    * add popover component
    
    * currentAdmin added
    
    * styling fixes
    
    * attempts on sleep
    
    * useeffect to change popover state variables before opening
    
    * minor cleanups
    
    * fix user role on user pages for deployment/builds
    
    Co-authored-by: Cindy Zhang <61183011+cindyxzhang@users.noreply.github.com>
    Co-authored-by: Gabe Espinosa <gabriel.je@berkeley.edu>

--- firebase/firestore/family.ts
+++ firebase/firestore/family.ts
@@ -1,15 +1,28 @@
-import firebaseApp from "../firebaseApp";
-import "firebase/firestore";
-import { Family, User } from "../../types/schema";
+import firebaseApp from '../firebaseApp';
+import 'firebase/firestore';
+import { Family, User } from '../../types/schema';
 import { getUser } from "./user";
 
 const db = firebaseApp.firestore();
-const familyCollection = db.collection("families");
+const familyCollection = db.collection('families');
+
+/**
+ * Returns the family data from firestore with the given userId
+ */
+export const getFamily = async (familyId: string): Promise<Family> => {
+    try {
+        const doc = await familyCollection.doc(familyId).get();
+        return doc.data() as Family;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
 
 /**
  * Returns all the families from firestore
  */
-export const getAllFamilies = async (): Promise<Family[]> => {
+ export const getAllFamilies = async (): Promise<Family[]> => {
   try {
     const allFamilies = await familyCollection.get();
     const promises: Promise<Family>[] = allFamilies.docs.map((doc) =>
@@ -21,6 +34,30 @@ export const getAllFamilies = async (): Promise<Family[]> => {
     console.warn(e);
     throw e;
   }
+};
+
+/**
+ * Adds the given family data to firestore
+ */
+export const addFamily = async (family: Family) => {
+    try {
+        await familyCollection.doc().set(family);
+    } catch (e) {
+        console.warn(e);
+        throw e;
+    }
+};
+
+/**
+ * Deletes the family from firestore with the given familyId
+ */
+export const deleteFamily = async (familyId: string) => {
+    try {
+        await familyCollection.doc(familyId).delete();
+    } catch (e) {
+        console.warn(e);
+        throw e;
+    }
 };
 
 const parseFamily = async (doc) => {
--- firebase/firestore/transaction.ts
+++ firebase/firestore/transaction.ts
@@ -1,5 +1,6 @@
 import firebaseApp from "../firebaseApp";
 import "firebase/firestore";
+import Query from 'firebase/firestore';
 import { Transaction } from "../../types/schema";
 
 const db = firebaseApp.firestore();
@@ -26,7 +27,7 @@ export const getTransaction = async (
 export const getAllTransactions = async (): Promise<Transaction[]> => {
   try {
     // query everything in the transaction collection
-    const allTransactions = await transactionsCollection.get();
+    const allTransactions = await transactionsCollection.orderBy("date", "desc").get();
     const promises: Promise<Transaction>[] = allTransactions.docs.map((doc) =>
       parseTransaction(doc)
     );
