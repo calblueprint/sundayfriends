4774ee1034c4f65fa04e7131e4efed06bd493bf1
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-11-22 14:12:05 -0800

    uncommented progress and small fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import { AuthProvider } from "../firebase/auth";
 import { AppProps } from "next/app";
 import { useEffect } from "react";
 import "../styles/globals.css";
-// import Router from "next/router";
-// import NProgress from "nprogress";
-// import "nprogress/nprogress.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  // Router.events.on("routeChangeStart", () => NProgress.start());
-  // Router.events.on("routeChangeComplete", () => NProgress.done());
-  // Router.events.on("routeChangeError", () => NProgress.done());
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
   useEffect(() => {
     // Remove the server-side injected CSS
     const jssStyles = document.querySelector("#jss-server-side");
