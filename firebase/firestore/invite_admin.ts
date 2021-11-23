f139728e9fc258d1c172bc2af62ec6ed1d4f1d08
alliu879 <alliu879@gmail.com>
2021-11-22 22:38:28 -0800

    Albert/invite admin modal (#26)
    created Invite Admin Modal
    emails sending functionality is functional
    ensured dynamic resizing as new invites are added
    added success modal
    form validation is functional but in a different PR
    
    Co-authored-by: Cindy Zhang <61183011+cindyxzhang@users.noreply.github.com>

--- firebase/firestore/invite_admin.ts
+++ firebase/firestore/invite_admin.ts
@@ -9,24 +9,29 @@ const adminInvitesCollection = db.collection("admin_invites");
  * Adds the given admin data to firestore
  */
 export const addAdminInvite = async (admin: AdminInvite) => {
-    try {
-        await adminInvitesCollection.doc().set(admin);
-    } catch (e) {
-        console.warn(e);
-        throw e;
+  try {
+    const query = await adminInvitesCollection
+      .where("email", "==", admin.email)
+      .get();
+    const docs = query.docs;
+    if (docs.length != 0) {
+      throw "email already registered!";
     }
+    await adminInvitesCollection.doc().set(admin);
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
 };
 
 /**
  * Deletes the admin from firestore with the given adminId
  */
 export const deleteAdmin = async (adminId: string) => {
-    try {
-        await adminInvitesCollection.doc(adminId).delete();
-    } catch (e) {
-        console.warn(e);
-        throw e;
-    }
+  try {
+    await adminInvitesCollection.doc(adminId).delete();
+  } catch (e) {
+    console.warn(e);
+    throw e;
+  }
 };
-
-
