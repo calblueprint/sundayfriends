57eac21587c492d7733677f278b6e16f3f043089
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-12-24 16:44:09 -0700

    removed profile info component from profile page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import styles from "./Profile.module.css";
 import firebaseAdmin from "../../firebase/firebaseAdmin";
@@ -7,11 +7,14 @@ import { Admin } from "../../types/schema";
 import { getAdmin } from "../../firebase/firestore/admin";
 import nookies from "nookies";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import {
   ProfileInfo,
   FieldInfo,
 } from "../../components/ProfileInfo/ProfileInfo";
 import Icon from "../../assets/Icon";
+import { useForm } from "react-hook-form";
+import { TextField } from "@mui/material";
 
 type ProfileSettingsPageProps = {
   currentAdmin: Admin;
@@ -19,64 +22,136 @@ type ProfileSettingsPageProps = {
 
 const ProfileSettingsPage: React.FunctionComponent<ProfileSettingsPageProps> =
   ({ currentAdmin }) => {
-    const aboutData: FieldInfo[] = [
-      {
-        iconName: "nameicon",
-        fieldName: "NAME",
-        fieldValue: "Cindy Zhang",
-      },
-      {
-        iconName: "singleperson",
-        fieldName: "ROLE",
-        fieldValue: "Executive Director",
-      },
-      {
-        iconName: "lastactive",
-        fieldName: "LAST ACTIVE",
-        fieldValue: "October 20, 2021",
-      },
-      {
-        iconName: "datejoined",
-        fieldName: "DATE JOINED",
-        fieldValue: "January 20, 2021",
-      },
-    ];
 
-    const loginInfo: FieldInfo[] = [
-      {
-        iconName: "email",
-        fieldName: "EMAIL",
-        fieldValue: "chloeisnotarealdog@gmail.com",
-      },
-      {
-        iconName: "phone",
-        fieldName: "PHONE #",
-        fieldValue: "(123) 456 - 7890",
-      },
-      {
-        iconName: "password",
-        fieldName: "PASSWORD",
-        fieldValue: "*******",
-      },
-    ];
+    const [adminName, setAdminName] = useState<string>(currentAdmin.name);
+    const [adminRole, setAdminRole] = useState<string>(currentAdmin.role);
+    const [adminEmail, setAdminEmail] = useState<string>(currentAdmin.email);
+    const [adminPhone, setAdminPhone] = useState<string>(currentAdmin.phone);
+
+    const [editingForm, setEditingForm] = useState<boolean>(false);
+    const [editingName, setEditingName] = useState<boolean>(false);
+    const [editingRole, setEditingRole] = useState<boolean>(false);
+    const [editingEmail, setEditingEmail] = useState<boolean>(false);
+    const [editingPhone, setEditingPhone] = useState<boolean>(false);
+
+    const { register, handleSubmit, reset } = useForm({
+      defaultValues: {
+        name: adminName,
+        role: adminRole,
+        email: adminEmail,
+        phone: adminPhone,
+      }
+    });
 
     const displayinfo = () => {
       return (
-        <div className={styles.profile}>
-          <div className={styles.namebar}>
-            <h1> Cindo Zhang </h1>
-            <Button variant="contained" className={styles.button}>
-              <Icon type="editpencil" className={styles.editicon}></Icon>
+        <div>
+          <div className={styles["namebar"]}>
+            <h1>{currentAdmin.name}</h1>
+            <Button variant="contained" className={styles["button"]}>
+              <Icon type="editpencil" />
               Edit
             </Button>
           </div>
-          <hr className={styles.hr}></hr>
-          <div className={styles.boxes}>
-            <div className={styles.box}>
-              <ProfileInfo data={aboutData} cardTitle="About" />
+          <hr className={styles["hr"]}></hr>
+          <div className={styles["boxes"]}>
+            <div className={styles["box"]}>
+              <Typography variant="h5" fontWeight="bold">
+                About
+              </Typography>
+              <hr></hr>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="nameicon" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    NAME
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  {adminName}
+                </Typography>
+              </div>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="singleperson" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    ROLE
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  {adminRole}
+                </Typography>
+              </div>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="lastactive" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    LAST ACTIVE
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  {currentAdmin.last_active}
+                </Typography>
+              </div>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="datejoined" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    DATE JOINED
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  {currentAdmin.created_at}
+                </Typography>
+              </div>
+              <br></br>
             </div>
-            <div className={styles.box}>
-              <ProfileInfo data={loginInfo} cardTitle="Login Details" />
+            <div className={styles["box"]}>
+              <Typography variant="h5" fontWeight="bold">
+                Login Details
+              </Typography>
+              <hr></hr>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="email" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    EMAIL
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  {adminEmail}
+                </Typography>
+              </div>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="phone" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    PHONE #
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  {adminPhone}
+                </Typography>
+              </div>
+              <br></br>
+              <div className={styles["info"]}>
+                <div className={styles["fields"]}>
+                  <Icon type="password" />
+                  <Typography variant="subtitle1" fontWeight="bold">
+                    PASSWORD
+                  </Typography>
+                </div>
+                <Typography variant="subtitle2" color="#131313">
+                  *********
+                </Typography>
+              </div>
+              <br></br>
             </div>
           </div>
         </div>
@@ -85,8 +160,8 @@ const ProfileSettingsPage: React.FunctionComponent<ProfileSettingsPageProps> =
 
     return (
       <Layout title="Profile">
-        <div className={styles.page}>
-          <div className={styles.pagetitle}>
+        <div className={styles["page"]}>
+          <div className={styles["pagetitle"]}>
             <Icon type="settings"></Icon>
             <h2>PROFILE SETTINGS</h2>
           </div>
